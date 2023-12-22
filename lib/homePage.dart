6242cba7bd4bc1cbcff48d7aa04481c6fa8aaae7
RJ2607 <rishijain2607@gmail.com>
2023-12-23 00:06:16 +0530

    Add webview_flutter dependency and implement video player

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MyHome extends StatefulWidget {
   MyHome({super.key});
@@ -8,19 +9,39 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomeState extends State<MyHome> {
+  final String videoUrl = 'https://vidsrc.to/embed/movie/tt2911666';
   @override
+  // String Data = 'https://vidsrc.to/embed/movie/tt15477488';
+
+  // VideoPlayerController? _controller;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _controller = VideoPlayerController.networkUrl(
+  //       Uri.http('vidsrc.to', 'embed/movie/tt15477488'))
+  //     ..initialize().then((_) {
+  //       setState(() {});
+  //     });
+  // }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('iWatch'),
-        backgroundColor: Colors.black,
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to iWatch',
-          style: TextStyle(fontSize: 24),
+        appBar: AppBar(
+          title: Text(
+            'iWatch',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          elevation: 0,
         ),
-      ),
-    );
+        body: WebView(
+          initialUrl: videoUrl,
+          javascriptMode: JavascriptMode.unrestricted,
+        ));
   }
 }
